f2aef9c57af4aa4986051817ca395da5936395b6
Bardaji <mikibardaji@hotmail.com>
2025-09-26 13:07:36 +0200

    ex5

--- A1/Condicionals/Condicionales/src/Ex5.java
+++ A1/Condicionals/Condicionales/src/Ex5.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Ex5 {
     /*
      * 5.	Programa 
@@ -5,6 +7,29 @@ public class Ex5 {
      * els visualitza en ordre ascendent.
      */
     public static void main(String[] args) {
-        
+      Scanner sc = new Scanner(System.in);
+      int v1, v2;
+      
+      System.out.print("Pon valor 1 ");
+      v1 = sc.nextInt();
+      System.out.print("Pon valor 2 ");
+      v2 = sc.nextInt();
+
+      if (v1>v2) //el mas grande es v1
+      {
+        System.out.println(v2 + "- " + v1);
+      }
+      else if (v1 < v2) //rl mas grande es v2 
+      {
+        System.out.println(v1 + " - " + v2);
+      }
+      else //son iguales
+      {
+        System.out.println(v1);
+      }
+
+
+
+
     }
 }
